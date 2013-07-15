f1969f52b89349bfce42dbe25db1eacbb0f26589
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-07-15 09:53:59 +0700

    update after folder changed

--- lexicon-server/src/views/pages/project.html.php
+++ lexicon-server/src/views/pages/project.html.php
@@ -1,9 +1,19 @@
+<div id="main">
+	<div class="main-holder">
+		<div class="clear-block">
+			<div class="tabs"></div>
+			<div class="region region-content">
+				<div class="block block-system" id="block-system-main">
+					<div class="content">
 
-<?php if (!$logged_in): ?>
-<!--  user not loged in, show only view-->
-		<div id="ViewerContent" class="readonly-view-view-only">
-			  <?php include ("listview.html.php"); ?>
-		</div>
+					<?php if (!$logged_in): ?>
+						<!--  user not loged in, show only view-->
+						<div id="ViewerContent" class="readonly-view-view-only">
+							
+						<?php include ("listview.html.php"); ?>
+						</div>
+						
+						
 
 <?php else: ?>
 <div id="ViewSwitch" class="dic-view-switch">
@@ -52,7 +62,7 @@
 		</script>
 	</div>
 		<div id="ViewerContent" class="readonly-view-view-with-edit">
-			  <?php include ($directory."/listview.php"); ?>
+			  <?php include ($directory."listview.php"); ?>
 		</div>
 	<div id="GWTContent"
 		style="height: 710px; width: 100%; padding-top: 10px;">
@@ -75,3 +85,9 @@
 	</div>
 
 	<?php endif; ?>
+	</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
