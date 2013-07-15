2a178c510c353999ab2a15f72550836f7151713e
cambell-prince <cambell.prince@gmail.com>
2013-07-15 14:52:27 +0700

    Site admin (user management) working.

--- lexicon-server/src/views/templates/header.html.php
+++ lexicon-server/src/views/templates/header.html.php
@@ -68,7 +68,7 @@
 							<a href="#"><img src="<?php echo $small_gravatar_url; ?>" style="float:left; position:relative; top:-6px; border:1px solid white; margin-right:10px" />Hi, <?php echo $user_name; ?></a>
 								<ul>
 									<?php if ($is_admin):?>
-									<li><a href="/app/sfadmin">Site Administration</a></li>
+									<li><a href="/app/site_admin">Site Administration</a></li>
 									<?php endif;?>
 									<li><a href="/app/userprofile">My Profile</a></li>
 									<li><a href="/auth/change_password">Change Password</a></li>
