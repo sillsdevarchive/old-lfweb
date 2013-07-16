f18e1b508167ad275bd8d12e6e0938fe727f934c
mikejbst <michael_cochran@sil.org>
2013-07-16 13:04:23 +0700

    commented correctly

--- lexicon-server/src/views/pages/frontpage.html.php
+++ lexicon-server/src/views/pages/frontpage.html.php
@@ -61,7 +61,7 @@
 					<p>16 July 2013</p>
 					<h3>Early experiments with language forge under way</h3>
 					<p>We are currently in the process of building the system to the point that we can start sharing dictionaries between language forge, <a href="http://wesay.palaso.org/" class="arrowed">WeSay</a>, and <a href="http://fieldworks.sil.org/flex/" class="arrowed">FieldWorks Language Explorer</a>. We are not ready for real testing yet but will let you know when you can start experimenting.</p>
-					// <p><a href="#" class="arrowed">Read Article</a></p>
+				    <!-- <p><a href="#" class="arrowed">Read Article</a></p> -->
 				</div>
 			</div>
 			
@@ -70,7 +70,7 @@
 					<p>2012</p>
 					<h3>Experimentation with environments</h3>
 					<p>In 2012 we implemented an equivalent to part of the interface using GWT and embedded this into a Drupal site. We were not able to get the Drupal part of the site working well however</p>
-					// <p><a href="#" class="arrowed">Read Article</a></p>
+					<!-- <p><a href="#" class="arrowed">Read Article</a></p> -->
 				</div>
 			</div>
 			
@@ -79,7 +79,7 @@
 					<p>2011</p>
 					<h3>Initiation of the Lexical Web app</h3>
 					<p>In 2011 we started experimenting with develpoment of a Web app that would integrate with the mature tools that SIL has created for lexical development. A plan was then created for further development.</p>
-					// <p><a href="#" class="arrowed">Read Article</a></p>
+					<!-- <p><a href="#" class="arrowed">Read Article</a></p> -->
 				</div>
 			</div>
 			
