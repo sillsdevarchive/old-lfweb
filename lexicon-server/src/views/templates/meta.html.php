3aa354340db1beb38a91a7c4947a3bb664460dc1
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-07-16 10:31:24 +0700

    removed test css

--- lexicon-server/src/views/templates/meta.html.php
+++ lexicon-server/src/views/templates/meta.html.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" media="screen" href="/css/superfish.css" />
 		<link rel="stylesheet" media="screen" href="/css/slides.css" />
 		<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-		<link rel="stylesheet" href="/css/lf-dictionary.css" />
+<!-- 		<link rel="stylesheet" href="/css/lf-dictionary.css" /> -->
 		<link rel="stylesheet" href="/css/lf.css" />
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
