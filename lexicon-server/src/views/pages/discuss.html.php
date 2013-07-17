bf6e3ada5292c6a9d9ce30b1cb9735d8ec76f7db
mikejbst <michael_cochran@sil.org>
2013-07-17 13:35:46 +0700

    Added and sized remaining pictures

--- lexicon-server/src/views/pages/discuss.html.php
+++ lexicon-server/src/views/pages/discuss.html.php
@@ -1,8 +1,8 @@
 <div class="sfcontainer banner">
-	<img src="/images/beach2.jpg" width="965px" height="360px" alt="on the beach" />
+	<img src="/images/womendiscussion.jpg" width="750px" height="494px" alt="on the beach" />
 </div>
 <div class="sfcontainer">
 			<h2>Discuss</h2>
-			<p>You can discuss Language Forge at our <a href="#">Google Group here.</a></p>
+			<p>You can discuss Language Forge at our <a href="#" target="_blank" >Google Group here.</a></p>
 			
 </div>
