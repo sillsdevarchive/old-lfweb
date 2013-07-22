cb0684a3d2aa8e21d03e61069e8eeab60285d5ec
mikejbst <michael_cochran@sil.org>
2013-07-22 13:04:24 +0700

    corrected error in bootstrap and updated some images

--- lexicon-server/src/views/pages/contribute.html.php
+++ lexicon-server/src/views/pages/contribute.html.php
@@ -1,5 +1,5 @@
 <div class="sfcontainer banner">
-	<img src="/images/reading.jpg" width="965px" height="360px" alt="on the beach" />
+	<img src="/images/youngleaders.jpg" width="400px" height="268px" alt="on the beach" />
 </div>
 <div class="sfcontainer">
 			<h2>Contribute Placeholder for App</h2>
