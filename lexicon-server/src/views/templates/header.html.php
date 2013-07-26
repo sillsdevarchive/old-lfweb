bad63fe3daee4f7c36da244c9ac96fe42e720c40
mikejbst <michael_cochran@sil.org>
2013-07-26 15:11:14 +0700

    Created the small Language Forge Logo and changed the small Scripture Forge Logo to a small Language Forge Logo

--- lexicon-server/src/views/templates/header.html.php
+++ lexicon-server/src/views/templates/header.html.php
@@ -10,7 +10,7 @@
 				
 				<div id="header-nav" class="left">
 				<?php if (!isset($is_static_page)):?>
-					<img align="left" style="margin: 2px 10px 0 0" src="/images/sf_logo_small.png" alt="Language Forge" width="27" height="36" />
+					<img align="left" style="margin: 2px 10px 0 0" src="/images/lf_logo_small.png" alt="Language Forge" width="27" height="36" />
 				<?php endif;?>
 					<ul class="sf-menu">
 						<li><a href="/">Home</a></li>
