e4123f9ca51a30f2a0f90f1742da0c2eca89e3dd
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-08-21 08:50:12 +0700

    disabled styled check box

--- lexicon-client/src/org/palaso/languageforge/client/lex/controls/ExtendedCheckBox.java
+++ lexicon-client/src/org/palaso/languageforge/client/lex/controls/ExtendedCheckBox.java
@@ -17,126 +17,126 @@ import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ExtendedCheckBox extends CheckBox {
-
-	boolean styled = false;
-	private Widget oldParent = null;
-	private InputElement checkBoxInputElement = null;
-	private LabelElement checkBoxLabelElement = null;
-	private static Integer refIdIndex = 0;
-	private boolean isChecked =false;
-
-	protected interface JsResource extends ClientBundle {
-		@Source("ExtendedCheckBox.js")
-		TextResource ExtendedCheckBoxJsResource();
-
-	}
-
-	//
-	// public interface Resources extends ClientBundle {
-	// public static final Resources INSTANCE =
-	// GWT.create(Resources.class);
-	//
-	// @NotStrict
-	// @Source("ExtendedCheckBox.css")
-	// public CssResource css();
-	// }
-
-	static {
-		// Resources.INSTANCE.css().ensureInjected();
-		JsResource bundle = GWT.create(JsResource.class);
-		JavascriptInjector
-				.inject(bundle.ExtendedCheckBoxJsResource().getText());
-	}
-
-	public ExtendedCheckBox() {
-		super();
-		init();
-	}
-
+//
+//	boolean styled = false;
+//	private Widget oldParent = null;
+//	private InputElement checkBoxInputElement = null;
+//	private LabelElement checkBoxLabelElement = null;
+//	private static Integer refIdIndex = 0;
+//	private boolean isChecked =false;
+//
+//	protected interface JsResource extends ClientBundle {
+//		@Source("ExtendedCheckBox.js")
+//		TextResource ExtendedCheckBoxJsResource();
+//
+//	}
+//
+//	//
+//	// public interface Resources extends ClientBundle {
+//	// public static final Resources INSTANCE =
+//	// GWT.create(Resources.class);
+//	//
+//	// @NotStrict
+//	// @Source("ExtendedCheckBox.css")
+//	// public CssResource css();
+//	// }
+//
+//	static {
+//		// Resources.INSTANCE.css().ensureInjected();
+//		JsResource bundle = GWT.create(JsResource.class);
+//		JavascriptInjector
+//				.inject(bundle.ExtendedCheckBoxJsResource().getText());
+//	}
+//
+//	public ExtendedCheckBox() {
+//		super();
+//		init();
+//	}
+//
 	public ExtendedCheckBox(SafeHtml label) {
 		super(label);
-		init();
+		//init();
 	}
 
 	public ExtendedCheckBox(String label) {
 		super(label);
-		init();
+		//init();
 	}
-
-	public ExtendedCheckBox(String label, DirectionEstimator dir) {
-		super(label, dir);
-		init();
-	}
-
-	public ExtendedCheckBox(String label, Direction dir) {
-		super(label, dir);
-		init();
-	}
-
-	public ExtendedCheckBox(String label, Boolean asHtml) {
-		super(label, asHtml);
-		init();
-	}
-
-	private void init() {
-		checkBoxInputElement = (InputElement) this.getElement().getFirstChild();
-		checkBoxLabelElement = (LabelElement) this.getElement().getLastChild();
-		checkBoxInputElement.setId("ex_chkbox_" + refIdIndex.toString());
-		refIdIndex++;
-	}
-
-	@Override
-	public void setValue(Boolean value) {
-		isChecked = value;
-		if (isAttached()) {
-			changeValueInternal(this.checkBoxInputElement.getId(), value);
-		}
-	}
-
-	@Override
-	public Boolean getValue() {
-		return isChecked;
-	}
-
-	@Override
-	public void onLoad() {
-		if (!styled) {
-			oldParent = this.getParent();
-			stylingCheckBox(checkBoxInputElement, checkBoxLabelElement);
-			styled = true;
-//		super.addAttachHandler(new Handler() {
+//
+//	public ExtendedCheckBox(String label, DirectionEstimator dir) {
+//		super(label, dir);
+//		init();
+//	}
+//
+//	public ExtendedCheckBox(String label, Direction dir) {
+//		super(label, dir);
+//		init();
+//	}
+//
+//	public ExtendedCheckBox(String label, Boolean asHtml) {
+//		super(label, asHtml);
+//		init();
+//	}
+//
+//	private void init() {
+//		checkBoxInputElement = (InputElement) this.getElement().getFirstChild();
+//		checkBoxLabelElement = (LabelElement) this.getElement().getLastChild();
+//		checkBoxInputElement.setId("ex_chkbox_" + refIdIndex.toString());
+//		refIdIndex++;
+//	}
+//
+//	@Override
+//	public void setValue(Boolean value) {
+//		isChecked = value;
+//		if (isAttached()) {
+//			changeValueInternal(this.checkBoxInputElement.getId(), value);
+//		}
+//	}
+//
+//	@Override
+//	public Boolean getValue() {
+//		return isChecked;
+//	}
+//
+//	@Override
+//	public void onLoad() {
+//		if (!styled) {
+//			oldParent = this.getParent();
+//			stylingCheckBox(checkBoxInputElement, checkBoxLabelElement);
+//			styled = true;
+////		super.addAttachHandler(new Handler() {
+////			
+////			@Override
+////			public void onAttachOrDetach(AttachEvent event) {
+////				if (event.isAttached())
+////				{
+////					changeValueInternal(checkBoxInputElement.getId(), isChecked);
+////				}
+////			}
+////		});
+//		}
+//	}
+//
+//	public HandlerRegistration addValueChangeHandler(
+//			final ValueChangeHandler<Boolean> handler) {
+//		
+//		return super.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
 //			
 //			@Override
-//			public void onAttachOrDetach(AttachEvent event) {
-//				if (event.isAttached())
-//				{
-//					changeValueInternal(checkBoxInputElement.getId(), isChecked);
-//				}
+//			public void onValueChange(ValueChangeEvent<Boolean> event) {
+//				isChecked=event.getValue().booleanValue();
+//				handler.onValueChange(event);
 //			}
 //		});
-		}
-	}
-
-	public HandlerRegistration addValueChangeHandler(
-			final ValueChangeHandler<Boolean> handler) {
-		
-		return super.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-			
-			@Override
-			public void onValueChange(ValueChangeEvent<Boolean> event) {
-				isChecked=event.getValue().booleanValue();
-				handler.onValueChange(event);
-			}
-		});
-	}
-
-	private static native void stylingCheckBox(InputElement inputElement,
-			LabelElement labelElement) /*-{
-		$wnd.stylingCheckBox(inputElement, labelElement);
-	}-*/;
-
-	private static native void changeValueInternal(String checkBoxID,
-			boolean value) /*-{
-		$wnd.checkCheckboxes(checkBoxID, value);
-	}-*/;
+//	}
+//
+//	private static native void stylingCheckBox(InputElement inputElement,
+//			LabelElement labelElement) /*-{
+//		$wnd.stylingCheckBox(inputElement, labelElement);
+//	}-*/;
+//
+//	private static native void changeValueInternal(String checkBoxID,
+//			boolean value) /*-{
+//		$wnd.checkCheckboxes(checkBoxID, value);
+//	}-*/;
 }
