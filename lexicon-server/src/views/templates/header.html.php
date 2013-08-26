2ce3d5f17369bba9cd1b765f50af4424f3c11f49
cyu <cyu-cyu@hotmail.com>
2013-08-26 14:30:54 +0700

    Fix 'My Projects' link in header.

--- lexicon-server/src/views/templates/header.html.php
+++ lexicon-server/src/views/templates/header.html.php
@@ -49,20 +49,11 @@
 				<?php if ($logged_in):?>
 					<div class="right">
 							<ul class="sf-menu">
-								<li><a href="#">My Projects</a>
+								<li><a href="/app/projects">My Projects</a>
 									<ul>
 									<?php foreach($projects as $project): ?>
 										<li><a href="<?php echo "/project?pid=" . $project['id']; ?>"><?php echo $project['projectname']; ?></a></li>
 									<?php endforeach;?>
-									<?php if ($is_admin): ?>
-										<?php if ($projects_count > 0): ?>
-										<li>---</li>
-										<?php endif; ?>
-										<?php foreach($all_projects as $project): ?>
-											<li><a href="<?php echo "/project?pid=" . $project['id']; ?>"><?php echo $project['projectname']; ?></a></li>
-										<?php endforeach;?>
-										
-									<?php endif; ?>
 									</ul>
 								</li>
 							</ul>
