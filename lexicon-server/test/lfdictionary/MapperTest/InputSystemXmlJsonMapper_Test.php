7dd60999dd4879840131af6776b72fa0ca4d3266
cyu-cyu <cyu-cyu@hotmail.com>
2013-08-29 14:55:57 +0700

    wip: fix MapperTest errors.

--- lexicon-server/test/lfdictionary/MapperTest/InputSystemXmlJsonMapper_Test.php
+++ lexicon-server/test/lfdictionary/MapperTest/InputSystemXmlJsonMapper_Test.php
@@ -1,9 +1,11 @@
 <?php
-require_once(dirname(__FILE__) . '/../testconfig.php');
-require_once(SIMPLETEST_PATH . 'autorun.php');
-require_once(LF_BASE_PATH . "/lfbase/Loader.php");
+use \libraries\lfdictionary\mapper\InputSystemXmlJsonMapper;
 
-use lfbase\mapper\InputSystemXmlJsonMapper;
+
+require_once(dirname(__FILE__) . '/../../testconfig.php');
+require_once(SIMPLETEST_PATH . 'autorun.php');
+require_once(LF_BASE_PATH . "Loader.php");
+
 
 class TestInputSystemXmlJsonMapper extends UnitTestCase {
 
