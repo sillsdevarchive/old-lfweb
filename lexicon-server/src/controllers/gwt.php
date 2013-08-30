9a165ffa8b2b514e4bc411e5c4f4387eaca94c4c
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-08-30 09:37:10 +0700

    added a mongodb validation in gwt controller

--- lexicon-server/src/controllers/gwt.php
+++ lexicon-server/src/controllers/gwt.php
@@ -12,6 +12,11 @@ class gwt extends base {
 	
 	public function view($page = 'main', $pid ='') {
 		
+		//some "favicon.ico" pass into here, find it out
+		if (strpos($pid,'.') !== false || strlen($pid)!== 24) {
+			error_log($pid. "is not a valid id");
+	    	return;
+		}
 		$data = array();
 		$data['gwt_page'] = $page;
 		$data['project_id'] = $pid;
@@ -43,6 +48,7 @@ class gwt extends base {
 			$this->_render_page("gwtpages/project", $data);
 		}
 	}
+
 }
 
 ?>
