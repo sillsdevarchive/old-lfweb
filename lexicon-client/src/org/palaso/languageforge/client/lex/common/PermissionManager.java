5b4882848cd8a30dbbf425cadb049f4e63f09e65
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-09-02 16:20:06 +0700

    permission table real only once when try to check it from table

--- lexicon-client/src/org/palaso/languageforge/client/lex/common/PermissionManager.java
+++ lexicon-client/src/org/palaso/languageforge/client/lex/common/PermissionManager.java
@@ -3,33 +3,31 @@ package org.palaso.languageforge.client.lex.common;
 import org.palaso.languageforge.client.lex.common.enums.DomainPermissionType;
 import org.palaso.languageforge.client.lex.common.enums.OperationPermissionType;
 import org.palaso.languageforge.client.lex.model.CurrentEnvironmentDto;
+import org.palaso.languageforge.client.lex.model.ProjectAccessDto;
 
 import com.google.gwt.core.client.JsArrayNumber;
 
 public final class PermissionManager {
 	/**
 	 * get permission by type
+	 * 
 	 * @return
 	 */
-	public static boolean getPermission(DomainPermissionType domain,
-			OperationPermissionType operation) {
-		ConsoleLog.log("Rights Check asked for: " +  domain.getValue() + "/" + operation.getValue());
-		if (CurrentEnvironmentDto.getCurrentProjectAccess() == null) {
+	public static boolean getPermission(DomainPermissionType domain, OperationPermissionType operation) {
+		ConsoleLog.log("Rights Check asked for: " + domain.getValue() + "/" + operation.getValue());
+		ProjectAccessDto projectAccess = CurrentEnvironmentDto.getCurrentProjectAccess();
+		if (projectAccess == null) {
 			ConsoleLog.log("Denied! No permission");
 			return false;
 		}
-		if (CurrentEnvironmentDto.getCurrentProjectAccess().getPermissions() == null
-				|| CurrentEnvironmentDto.getCurrentProjectAccess()
-						.getPermissions().length() == 0) {
+		if (projectAccess.getPermissions() == null || projectAccess.getPermissions().length() == 0) {
 			ConsoleLog.log("Denied! No permission");
 			return false;
 		}
-		JsArrayNumber permissionArray = CurrentEnvironmentDto
-				.getCurrentProjectAccess().getPermissions();
+		JsArrayNumber permissionArray = projectAccess.getPermissions();
 		for (int i = 0; i < permissionArray.length(); i++) {
 			int permission = (int) permissionArray.get(i);
-			if (Integer.parseInt(domain.getValue())
-					+ Integer.parseInt(operation.getValue()) == permission) {
+			if (Integer.parseInt(domain.getValue()) + Integer.parseInt(operation.getValue()) == permission) {
 				ConsoleLog.log("Accpet!");
 				return true;
 			}
@@ -38,5 +36,4 @@ public final class PermissionManager {
 		return false;
 	}
 
-	
 }
