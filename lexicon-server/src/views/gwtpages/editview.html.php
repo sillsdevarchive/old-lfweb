e55b58f66f4946f592480f37150034d5f0c952f1
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-09-04 16:08:12 +0700

    GWT bootstrap added

--- lexicon-server/src/views/gwtpages/editview.html.php
+++ lexicon-server/src/views/gwtpages/editview.html.php
@@ -69,7 +69,7 @@
 							<?php include ("listview.html.php"); ?>
 						</div>
 						<div id="GWTContent"
-							style="height: 710px; width: 100%; padding-top: 10px;"
+							style="height: 720px; width: 100%;"
 							targetPage= "<?php echo $gwt_page; ?>">
 
 							<?php if ($canShowEditor): ?>
