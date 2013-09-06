a6cb6aa9ff4c7849cc1807b5543fb40c2b9a3ece
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-09-06 11:11:14 +0700

    some basic stuff for  Language Depot import UI

---
+++ lexicon-server/src/angular-app/dpimport/js/controllers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* Controllers */
+
+angular.module(
+	'dpimport.controllers',
+	[ 'lf.services', 'ui.bootstrap' ]
+)
+.controller('UserCtrl', ['$scope', 'userService', function UserCtrl($scope, userService) {
+
+	$scope.record = {};
+	$scope.success = {
+		'state':false,
+		'message':''
+	};
+	$scope.usernameok = true;
+	$scope.usernameexist = false;
+	$scope.usernameloading = false;
+	$scope.record.id = '';
+	$scope.record.password = '';
+	$scope.createUser = function(record) {
+		record.captcha_challenge = record.captcha.challenge;
+		record.captcha_response = record.captcha.response;
+		userService.create(record, function(result) {
+			if (result.ok) {
+			
+			} else {
+				$scope.success.state = false;
+				$scope.success.message = "An error occurred in the signup process: ";
+			}
+		});
+		return true;
+	};
+}])
+;
