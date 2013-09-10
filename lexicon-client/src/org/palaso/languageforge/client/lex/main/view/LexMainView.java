37525c54eeae84b4524caae1a3b15fe7c547da93
ZhengXinCN <zheng.xin.cn@gmail.com>
2013-09-10 10:48:56 +0700

    Error info box styling

--- lexicon-client/src/org/palaso/languageforge/client/lex/main/view/LexMainView.java
+++ lexicon-client/src/org/palaso/languageforge/client/lex/main/view/LexMainView.java
@@ -2,6 +2,10 @@ package org.palaso.languageforge.client.lex.main.view;
 
 import org.palaso.languageforge.client.lex.main.presenter.LexMainPresenter;
 
+import com.github.gwtbootstrap.client.ui.Alert;
+import com.github.gwtbootstrap.client.ui.constants.AlertType;
+import com.github.gwtbootstrap.client.ui.event.ClosedEvent;
+import com.github.gwtbootstrap.client.ui.event.ClosedHandler;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -61,58 +65,33 @@ public class LexMainView extends Composite implements LexMainPresenter.ILexMainV
 		if (text == null) {
 			text = "NULL";
 		}
-		final SimplePanel messagePanel = new SimplePanel();
+		final Alert messagePanel = new Alert();
+		messagePanel.setText(text);
+		messagePanel.setAnimation(true);
+		messagePanel.setClose(true);
 		if (success) {
-			messagePanel.setStyleName("feedbackSuccess feedback_message");
+			messagePanel.setType(AlertType.SUCCESS);
 		} else {
-			messagePanel.addStyleName("feedbackError feedback_message");
+			messagePanel.setType(AlertType.ERROR);
 		}
 
-		FlowPanel rowCon = new FlowPanel();
-		SimplePanel messageInnerPanel = new SimplePanel();
-		messageInnerPanel.setStyleName("lex-dc-row");
-		rowCon.getElement().setId("firefox-bug-fix");
-		messageInnerPanel.add(rowCon);
-
-		SimplePanel btnPanel = new SimplePanel();
-		SimplePanel widgetPanel = new SimplePanel();
-		SimplePanel endingSpacePanel = new SimplePanel();
-		btnPanel.setStyleName("lex-dc-column feedback_message_closer_cell");
-		btnPanel.setWidth("30px");
-		widgetPanel.setStyleName("lex-dc-column");
-		widgetPanel.getElement().setId("c2");
-
-		endingSpacePanel.setStyleName("lex-dc-column");
-		endingSpacePanel.getElement().setId("c3");
-
-		Button btnClose = new Button();
-		btnClose.addStyleName("small-button");
-		btnClose.setWidth("20");
-		btnClose.setText("X");
-		btnClose.addClickHandler(new ClickHandler() {
+		messagePanel.addClosedHandler(new ClosedHandler() {
 			@Override
-			public void onClick(ClickEvent event) {
+			public void onClosed(ClosedEvent closedEvent) {
 				messageList.remove(messagePanel);
+				
 			}
 		});
-
 		if (success) {
 			// will close message automatic
 			Timer t = new Timer() {
 				public void run() {
-					messageList.remove(messagePanel);
+					messagePanel.close();
 				}
 			};
 			// close message box in 5 seconds.
 			t.schedule(5000);
 		}
-		widgetPanel.add(new HTMLPanel(text));
-		btnPanel.add(btnClose);
-
-		rowCon.add(btnPanel);
-		rowCon.add(widgetPanel);
-		rowCon.add(endingSpacePanel);
-		messagePanel.add(messageInnerPanel);
 		messageList.add(messagePanel);
 	}
 
