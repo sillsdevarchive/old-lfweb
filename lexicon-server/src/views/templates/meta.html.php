ce2ea6fc660ef78a22e9244402dc37325d3fa6c8
megahirt <chris@hirtfamily.net>
2013-12-09 14:52:22 +0700

    added font awesome as a local resource

--- lexicon-server/src/views/templates/meta.html.php
+++ lexicon-server/src/views/templates/meta.html.php
@@ -12,8 +12,8 @@
 		<link rel="stylesheet" href="/css/lf.css" />
 		<link rel="stylesheet" href="/css/lexiquepro.css" />
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
+		<link href="/css/font-awesome.css" rel="stylesheet">
 		<!--
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-		<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
   -->
 		<script src="/js/lib/jquery-1.8.3.min.js"></script>
